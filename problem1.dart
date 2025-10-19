acc66439680ecce2eb1020f91e21af17705999bd
Habib Shamun <habibsalehinshamun@gmail.com>
2025-10-19 22:21:05 +0600

    problem 1 done

---
+++ problem1.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  List<String> names=[];
+  stdout.write("How many names do you want to enter:");
+  int? n=int.parse(stdin.readLineSync()!);
+  for(int i=0; i<n;i++) {
+    stdout.write("Name $i: ");
+    String? m=stdin.readLineSync()!;
+    names.add(m);
+  }
+
+  print("The names as a list are: ");
+  print(names);
+  print("Printing the strings from the list individually.");
+    for(int i=0; i<n;i++) {
+    print("Name $i: ${names.elementAt(i)}");
+  }
+
+}

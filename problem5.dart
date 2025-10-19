c77a7d223993b3cb9b1a845d0ab26a45191f7ed7
Habib Shamun <habibsalehinshamun@gmail.com>
2025-10-19 23:49:26 +0600

    problem 5 done

---
+++ problem5.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  List<String> friends=[];
+  print("The name of seven of y friends are:");
+  for(int i=1;i<=7;i++) {
+    stdout.write("Friend $i: ");
+    String? friend=stdin.readLineSync()!;
+    friends.add(friend);
+  }
+
+  List<String> firendsWithA=friends.where((e)=>e.toLowerCase().startsWith("a")).toList();
+  print("All of my friends:");
+  print(friends);
+  print("Friends with a in the beginning of their names:");
+  print(firendsWithA);
+}
